2d81418d8b26ed0430839aa10ff95e49785cd3f4
Terry Lockett <terryjlockett@gmail.com>
2022-12-14 13:25:12 -0400

    Day 6 part 2

--- day06/src/main/java/ca/terrylockett/aoc2022/day06/Day06Runner.java
+++ day06/src/main/java/ca/terrylockett/aoc2022/day06/Day06Runner.java
@@ -2,8 +2,6 @@ package ca.terrylockett.aoc2022.day06;
 
 import ca.terrylockett.aoc2022.common.InputFileHelper;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -15,8 +13,11 @@ public class Day06Runner {
         String input = Files.readString(Path.of(file));
         
         
-        int part1 = TuningTrouble.findStartOfPacketIndex(input);
+        int part1 = TuningTrouble.findStartOfPacketIndexPart1(input);
         System.out.println("Day06 Part1: " + part1);
+
+        int part2 = TuningTrouble.findStartOfPacketIndexPart2(input);
+        System.out.println("Day06 Part2: " + part2);
     }
     
 }
--- day06/src/test/java/ca/terrylockett/aoc2022/day06/TestDay06.java
+++ day06/src/test/java/ca/terrylockett/aoc2022/day06/TestDay06.java
@@ -10,36 +10,62 @@ public class TestDay06 {
     @Test
     public void testPart01() throws Exception{
         
-        assertEquals(7, TuningTrouble.findStartOfPacketIndex("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+        assertEquals(7, TuningTrouble.findStartOfPacketIndexPart1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
         
     }
 
     @Test
     public void testPart01_b() throws Exception{
 
-        assertEquals(5, TuningTrouble.findStartOfPacketIndex("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+        assertEquals(5, TuningTrouble.findStartOfPacketIndexPart1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
 
     }
 
     @Test
     public void testPart01_c() throws Exception{
 
-        assertEquals(6, TuningTrouble.findStartOfPacketIndex("nppdvjthqldpwncqszvftbrmjlhg"));
+        assertEquals(6, TuningTrouble.findStartOfPacketIndexPart1("nppdvjthqldpwncqszvftbrmjlhg"));
 
     }
 
     @Test
     public void testPart01_d() throws Exception{
 
-        assertEquals(10, TuningTrouble.findStartOfPacketIndex("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+        assertEquals(10, TuningTrouble.findStartOfPacketIndexPart1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
 
     }
 
     @Test
     public void testPart01_e() throws Exception{
 
-        assertEquals(11, TuningTrouble.findStartOfPacketIndex("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+        assertEquals(11, TuningTrouble.findStartOfPacketIndexPart1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
 
     }
     
+    
+    @Test
+    public void testPart2_a() throws Exception {
+        assertEquals(19, TuningTrouble.findStartOfPacketIndexPart2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
+    }
+
+
+    @Test
+    public void testPart2_b() throws Exception {
+        assertEquals(23, TuningTrouble.findStartOfPacketIndexPart2("bvwbjplbgvbhsrlpgdmjqwftvncz"));
+    }
+
+    @Test
+    public void testPart2_c() throws Exception {
+        assertEquals(23, TuningTrouble.findStartOfPacketIndexPart2("nppdvjthqldpwncqszvftbrmjlhg"));
+    }
+
+    @Test
+    public void testPart2_d() throws Exception {
+        assertEquals(29, TuningTrouble.findStartOfPacketIndexPart2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
+    }
+
+    @Test
+    public void testPart2_e() throws Exception {
+        assertEquals(26, TuningTrouble.findStartOfPacketIndexPart2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    }
 }
